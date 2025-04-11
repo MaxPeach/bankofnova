74e138f0f18be1c78d5ad23c5b586296a93b5712
Max Peach <maxpeach742@gmail.com>
2025-04-11 14:03:37 -0300

    feat: update db for deployment

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,10 +3,11 @@ import mysql from 'mysql2/promise';
 import { RowDataPacket } from 'mysql2';
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'nextjs_user',
-  password: 'strongpassword',
-  database: 'bankofnova',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
